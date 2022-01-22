e13ddf0c0876505e29400f0c043eb22394fed1bc
ongun demirag <ongun.demirag@gmail.com>
2022-01-22 19:33:29 +0300

    com

---
+++ EX1-SettingUpGit/git.tsx
@@ -0,0 +1,15 @@
+/*
+// check git version
+git --version 
+// git user name add
+git config --global user.name "Ongun Demirag"
+// git user email add
+git config --global user.email "ongun.demirag@gmail.com"
+// git list configuration
+git config --list
+
+
+
+
+
+*/
